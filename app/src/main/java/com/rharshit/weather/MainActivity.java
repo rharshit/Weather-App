f5fd796c8a3efc9a5d801c6a43d0d1e91f2c4723
rharshit <rharshit98@gmail.com>
2019-03-27 23:53:54 +0530

    UI: Implement main layout

--- app/src/main/java/com/rharshit/weather/MainActivity.java
+++ app/src/main/java/com/rharshit/weather/MainActivity.java
@@ -1,13 +1,47 @@
 package com.rharshit.weather;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etSearch;
+    private ListView lvSearch;
+
+    private Context mContext;
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+
+        etSearch = findViewById(R.id.et_search_city);
+        lvSearch = findViewById(R.id.list_search_cities);
+
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String city = etSearch.getText().toString();
+                Log.d(TAG, "afterTextChanged: City: " + city);
+            }
+        });
     }
 }
